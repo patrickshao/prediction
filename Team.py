5f81798469d379b50484b873bec361a1070c5724
auberginekenobi <osc12013@mymail.pomona.edu>
2014-12-08 02:02:00 -0800

    Broken code, weird python error.

--- Team.py
+++ Team.py
@@ -1,24 +1,34 @@
 class Team:
-	def __init__(self,name):
-		self.name = name
-		#scoresDict[opposing team] = [(team score, opposing score)...]
-		self.scoresDict = dict() 
-		#scoresList[chronological by time] = [[team, (team score, opposing score)]...]
-		self.scoresList = list()
-		self.totalGames = 0
-		self.totalWins = 0
-		self.premier = False
+    def __init__(self,name):
+        self.name = name
+        #scoresDict[opposing team] = [(team score, opposing score)...]
+        self.scoresDict = dict() 
+        #scoresList[chronological by time] = [[team, (team score, opposing score)]...]
+        self.scoresList = list()
+        self.totalGames = 0
+        self.totalWins = 0
+        self.premier = False
 
-	def getRecentGames(self,num):
-		temp = list()
-		if len(scoresList)-1 > num:
-			for i in range(num,len(scoresList)):
-				temp.append(scoresList(i))
-		return temp
+    def getRecentGames(self,num):
+        temp = list()
+        if len(self.scoresList)-1 > num:
+            for i in range(num,len(scoresList)):
+                temp.append(scoresList(i))
+        return temp
 
-	def historyVs(self,team):
-		return scoresDict[team]
+    def historyVs(self,team):
+        print scoresDict
+        return self.scoresDict[team]
 
+    def wins(self,current):
+        tempWin = 0
+        for r in range(0,current):
+            (opponent,scoreTup) = scoresList[r]
+            (tScore,oScore) = scoreTup
+            if tScore > oScore:
+                tempWin+=1
+        return tempWin
+                    
 def makeTeam(name):
-	team = Team(name)
-	return team
+    team = Team(name)
+    return team
