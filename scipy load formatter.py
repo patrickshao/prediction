e2c3137e438735aa06c069b6465ae34177591232
auberginekenobi <osc12013@mymail.pomona.edu>
2014-12-10 19:52:22 -0800

    workin on scipy load formatter

--- scipy load formatter.py
+++ scipy load formatter.py
@@ -5,6 +5,8 @@
 #map labels to data
 #convert all data to numbers
 #which labels do we want to use
+import string
+import csv
 
 #read the team names from file
 def readTeamList():
@@ -32,16 +34,47 @@ def readLabels(r):
     return labellist
 
 #starting features needed: ht index, at index, ht goals, at goals, ft result (label)
-#                       
+#some necessary dictionaries
+FTRdict={'H':'2','D':'1','A':'0'}
 
 #format csv to standardized label order
 #convert strings to numbers
-def formatData(r,w):
-    pass
+def formatData(rcsv,wcsv):
+    reader=csv.DictReader(rcsv)
+    for row in reader:
+        error=False
+        writelist=[]#list of strings to write to the file.
+
+        #format result
+        # Home win =2, tie=1, home loss=0
+        result=row['FTR']
+        writelist.append(FTRdict[result])
+        
+        #format date
+        date= row['Date']
+        date=date.strip()
+        date=date.split('/')
+        for item in date:
+            writelist.append(item)
+
+        #write league
+        #0 indicates premier
+        writelist.append('0')
+
+        #write goals scored
+        writelist.append(str(row['FTHG']))
+        writelist.append(str(row['FTAG']))
+        
+        print writelist
+
+            
+                
 
 
 #main method
-def run():
-    r=open('2014 raw.csv')
-    labellist=readLabels(r)
-    print labellist
+def run(year):
+    fname="raw/"+str(year)+" raw.csv"
+    with open(fname,'r') as f:
+        formatData(f,3)
+    #labellist=readLabels(r)
+    #print labellist
