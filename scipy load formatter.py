36cfd6966a3a4ae913e81d2b1170fce79ac59df1
auberginekenobi <osc12013@mymail.pomona.edu>
2014-12-10 22:20:54 -0800

    makin the formatter, created alldata w single season
    alldata with an entire season's games (year 2000)

--- scipy load formatter.py
+++ scipy load formatter.py
@@ -34,21 +34,52 @@ def readLabels(r):
     return labellist
 
 #starting features needed: ht index, at index, ht goals, at goals, ft result (label)
-#some necessary dictionaries
-FTRdict={'H':'2','D':'1','A':'0'}
+#some necessary data structures
+FTRhdict={'H':'2','D':'1','A':'0'}
+FTRadict={'H':'0','D':'1','A':'2'}
+r=open('teamNames.txt')
+teamlist=[]
+for line in r:
+    line=line.strip()
+    words=line.split(',')
+    teamlist=words
+    break
+keys=['FTHG','FTAG','HS','AS','HST','AST','HHW','AHW','HC','AC','HF','AF',
+      'HO','AO','HY','AY','HR','AR']
 
 #format csv to standardized label order
 #convert strings to numbers
 def formatData(rcsv,wcsv):
     reader=csv.DictReader(rcsv)
+    writer=csv.writer(wcsv)
+    error=False
     for row in reader:
-        error=False
         writelist=[]#list of strings to write to the file.
+        awritelist=[]
 
         #format result
         # Home win =2, tie=1, home loss=0
         result=row['FTR']
-        writelist.append(FTRdict[result])
+        writelist.append(FTRhdict[result])
+        awritelist.append(FTRadict[result])
+
+        #format team names
+        h=None
+        a=None
+        for i in range(len(teamlist)):
+            if row['HomeTeam'] == teamlist[i]:
+                h=i
+            elif row['AwayTeam'] == teamlist[i]:
+                a=i
+            if a!=None and h!=None:
+                break
+        if a==None or h==None:
+            error=True
+            break
+        writelist.append(str(h))
+        writelist.append(str(a))
+        awritelist.append(str(a))
+        awritelist.append(str(h))
         
         #format date
         date= row['Date']
@@ -56,16 +87,26 @@ def formatData(rcsv,wcsv):
         date=date.split('/')
         for item in date:
             writelist.append(item)
+            awritelist.append(item)
 
         #write league
         #0 indicates premier
-        writelist.append('0')
+        #writelist.append('0')
 
         #write goals scored
-        writelist.append(str(row['FTHG']))
-        writelist.append(str(row['FTAG']))
-        
+        writelist.append(row['FTHG'])
+        writelist.append(row['FTAG'])
+        awritelist.append(row['FTAG'])
+        awritelist.append(row['FTHG'])
+
+        #write shots, shots on target, shots on woodwork
+        #writelist.append(row['HS'])
+
+        #write
         print writelist
+        writer.writerow(writelist)
+        writer.writerow(awritelist)
+            
 
             
                 
@@ -75,6 +116,7 @@ def formatData(rcsv,wcsv):
 def run(year):
     fname="raw/"+str(year)+" raw.csv"
     with open(fname,'r') as f:
-        formatData(f,3)
+        with open('alldata.csv','w') as g:
+            formatData(f,g)
     #labellist=readLabels(r)
     #print labellist
