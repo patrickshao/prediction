cd4904baeff303882e3f777443f44c1b0793b0f4
patrickshao <shao.pat@gmail.com>
2014-12-13 21:44:08 -0800

    loadData now properly processes test and validate csv files

--- loadData.py
+++ loadData.py
@@ -81,9 +81,9 @@ def makeCSV(inputName,outputName,team,opposing,day,month,year,numIter,numPast):
 def run():
 	makeCSV("trainingset.csv","train.csv","1","2",4,3,9999,100,2)
 	teamDict = dict()
-	makeCSV("validationset.csv","validate.csv","1","2",4,3,2010,10,5)
+	makeCSV("validationset.csv","validate.csv","1","2",4,3,9999,10,2)
 	teamDict = dict()
-	makeCSV("testingset.csv","test.csv","1","2",4,3,2010,10,5)
+	makeCSV("testingset.csv","test.csv","1","2",4,3,9999,10,2)
 	
 
 run()	
